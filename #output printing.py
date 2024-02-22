8ed811d12cb96ab067652643d58b3788ee923d77
muthomi <mikebluez196@gmail.com>
2024-02-22 10:47:30 +0300

    first commit

---
+++ #output printing.py
@@ -0,0 +1,12 @@
+#output printing
+print("good Morning",end=" ")
+print('it is a lovely day')
+print('New Year',2024,"see you soon,",sep='.')
+#output formating
+x=5
+y=7
+print('the value of x is {} and y is {}'.format(x,y))
+#python input.
+num=int (input('Enter the value:'))
+print('you entered', num)
+print('the data type is', type(num))
